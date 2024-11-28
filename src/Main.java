8f66690af2989644eb71f960d36d9ae369214f99
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-11-28 16:50:48 +0000

    setVisibility to true

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,500);
+        frame.setVisible(true);
 
     }
 }
