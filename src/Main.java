0cafdbd6aea6b5f587c1aea1947fea88a6e46ac0
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-11-28 16:52:44 +0000

    add a JLabel

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
+        JLabel label = new JLabel();
+
+
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,500);
