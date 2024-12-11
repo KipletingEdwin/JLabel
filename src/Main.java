2b0a1c1cf16560b10992b3202a887b725de5e900
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-12-11 09:20:04 +0000

    setForeground to 0x00FF00

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -12,6 +13,7 @@ public class Main {
         label.setIcon(image);
         label.setHorizontalTextPosition(JLabel.CENTER);
         label.setVerticalTextPosition(JLabel.TOP);
+        label.setForeground(new Color(0x00FF00));
 
 
         JFrame frame = new JFrame();
