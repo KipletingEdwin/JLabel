39aec28e1d806aef085287595798ff6cf91b2768
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-12-11 09:11:43 +0000

    Add an image Icon

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
+        ImageIcon image = new ImageIcon("airbus.jpeg");
+
         JLabel label = new JLabel();
         label.setText("Bro, dou you even code?");
 
